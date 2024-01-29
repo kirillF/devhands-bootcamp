bbaa62b934d8ae5d1d87858869acdc3a0632f49f
KirillF <kirill.filimonov@gmail.com>
2024-01-30 00:34:04 +0200

    data queries added

--- main.py
+++ main.py
@@ -1,49 +1,37 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Request, Query
 from fastapi.middleware.cors import CORSMiddleware
-from enum import Enum
-import time
 import ctypes
-from datetime import datetime
 from resource import getrusage, RUSAGE_SELF
 import platform
 from dotenv import load_dotenv
-import psycopg
-from psycopg import AsyncConnection
-import aioredis
+from psycopg_pool import AsyncConnectionPool
 import os
+import math
+import asyncio
+import redis.asyncio as aioredis
+from contextlib import asynccontextmanager
+
 
 load_dotenv()
 
 
 origins = ["*"]
 
-app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+conn_info = f'host={os.getenv("POSTGRES_HOST")} port={os.getenv("POSTGRES_PORT")} dbname={os.getenv("POSTGRES_DB")} user={os.getenv("POSTGRES_USER")} password={os.getenv("POSTGRES_PASSWORD")}'
+host = os.getenv("REDIS_HOST")
+port = os.getenv("REDIS_PORT")
 
 
-async def get_conn():
-    pool = await psycopg.create_pool(
-        database=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
-        host=os.getenv("POSTGRES_HOST"),
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.redis = await aioredis.from_url(
+        f"redis://{host}:{port}", encoding="utf-8", decode_responses=True
     )
-    async with pool.acquire() as conn:
-        yield conn
-
-
-async def get_redis():
-    host = os.getenv("REDIS_HOST")
-    port = os.getenv("REDIS_PORT")
-    redis = await aioredis.create_redis_pool(f"redis://{host}:{port}")
-    return redis
+    app.async_pool = AsyncConnectionPool(conninfo=conn_info)
+    yield
+    await app.async_pool.close()
+    await app.redis.close()
 
 
 if platform.system() == "MacOS":
@@ -52,6 +40,17 @@ elif platform.system() == "Linux":
     libc = ctypes.CDLL("libc.so.6")
 else:
     libc = ctypes.CDLL(None)
+
+app = FastAPI(lifespan=lifespan)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -68,85 +67,93 @@ async def abuse(cpu: int = 0, sleep: int = 0, usleep: int = 0):
         result = 0
         k = 0
         while True:
-            for i in range(k):
-                for j in range(i):
-                    result += (k**17 + i**13 + j**11) % (10**17 + 7)
-            k = (k + 1) % (10**17 + 7)
-
+            k = (k + 1) % 1000000
+            result += math.exp(math.sin(math.radians(k))) * math.log1p(k)
             curr = getrusage(RUSAGE_SELF).ru_utime
             if curr - start >= cpu / 1000:
                 break
         end = getrusage(RUSAGE_SELF).ru_utime
-        response["cpu"] = {
+        return {
             "duration": str(cpu) + "ms",
             "status": "ok",
             "start": start,
             "end": end,
         }
 
-    if cpu > 0:
-        cpu_abuse(cpu)
-
-    if sleep > 0:
+    async def _sleep(sleep):
         start = getrusage(RUSAGE_SELF).ru_utime
-        time.sleep(sleep / 1000)
+        asyncio.sleep(sleep / 1000)
         end = getrusage(RUSAGE_SELF).ru_utime
-        response["sleep"] = {
+        return {
             "duration": str(sleep) + "ms",
             "status": "ok",
             "start": start,
             "end": end,
         }
 
-    if usleep > 0:
+    async def _usleep(usleep):
         start = getrusage(RUSAGE_SELF).ru_utime
         libc.usleep(usleep * 1000)
         end = getrusage(RUSAGE_SELF).ru_utime
-        response["usleep"] = {
+        return {
             "duration": str(usleep) + "ms",
             "status": "ok",
             "start": start,
             "end": end,
         }
 
+    if cpu > 0:
+        cpu = await asyncio.create_task(cpu_abuse(cpu))
+        response["cpu"] = cpu
+
+    if sleep > 0:
+        sleep = await asyncio.create_task(_sleep(sleep))
+        response["sleep"] = sleep
+
+    if usleep > 0:
+        usleep = await asyncio.create_task(_usleep(usleep))
+        response["usleep"] = usleep
+
     return response
 
 
 @app.get("/warmup")
-async def warmup(conn: AsyncConnection = Depends(get_conn)):
-    for i in range(1, 6):
-        item = await get_redis().get(f"item:{i}")
-        if item is None:
-            item = await read_item(i, conn)
-            await get_redis().set(f"item:{i}", item)
+async def warmup(request: Request):
+    r = request.app.redis
+    async with request.app.async_pool.connection() as conn:
+        for i in range(1, 6):
+            item = await r.get(f"item:{i}")
+            if item is None:
+                item = db_item_to_dict(await read_item(i, conn))
+                await r.set(f"item:{i}", str(item))
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, conn: AsyncConnection = Depends(get_conn)):
-    item = await conn.fetchrow("SELECT * FROM items WHERE id = $1", item_id)
-    return item
+async def read_item(item_id: int, request: Request, from_cache: bool = False):
+    if from_cache:
+        item = await request.app.redis.get(f"item:{item_id}")
+        return item
+    else:
+        async with request.app.async_pool.connection() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(f"SELECT * FROM items WHERE id = {item_id}")
+                item = await cur.fetchone()
+                return item
 
 
 @app.get("/items")
-async def read_items(conn: AsyncConnection = Depends(get_conn)):
+async def read_items(request: Request, from_cache: bool = False):
     response = []
     for i in range(1, 6):
-        result = await read_item(i, conn)
-        response.append(result)
+        item = read_item(i, request, from_cache)
+        response.append(item)
     return response
 
 
-@app.get("/items/cache/{item_id}")
-async def get_item_from_cache(item_id: int):
-    item = await get_redis().get(f"item:{item_id}")
-    return item
-
-
-@app.get("/items/cache")
-async def read_items_from_cache():
-    redis = await get_redis()
-    items = []
-    for i in range(1, 6):  # Fetch items with IDs from 1 to 5
-        item = await redis.get(f"item:{i}")
-        items.append(item)
-    return items
+def db_item_to_dict(item):
+    return {
+        "id": item[0],
+        "name": item[1],
+        "description": item[2],
+        "date": item[3],
+    }
